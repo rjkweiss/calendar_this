416ae418a26f545ecb3530e31b02a67c378b3103
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-03-26 19:05:45 -0500

    displaying appointments on a table

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
 from flask import Blueprint, render_template
+import psycopg2
+import os
 
 bp = Blueprint('main', __name__, '/')
+
+CONNECTION_PARAMETERS = {
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'dbname': os.environ.get('DB_NAME'),
+    'host': os.environ.get('DB_HOST')
+}
 
 # display main page
 @bp.route("/")
 def main():
-    return render_template('main.html')
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute("""
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+                """
+            )
+            rows = curs.fetchall()
+
+            return render_template('main.html', rows=rows)
