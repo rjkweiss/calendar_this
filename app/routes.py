5d630a2ad113aa388c864ad4dddf4312c4d2c181
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-03-26 21:21:47 -0500

    show appointments for a specific day only, default being current date

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import psycopg2
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
 from app.forms import AppointmentForm
 
@@ -15,9 +15,9 @@ CONNECTION_PARAMETERS = {
     'host': os.environ.get('DB_HOST')
 }
 
-# display main page
-@bp.route("/", methods=['GET', 'POST'])
-def main():
+# display the appointments for a given day
+@bp.route('/<int:year>/<int:month>/<int:day>', methods=['GET', 'POST'])
+def daily(year, month, day):
     # instantiate the form
     form = AppointmentForm()
 
@@ -44,6 +44,9 @@ def main():
         # redirect to '/'
         return redirect('/')
 
+    selected_day = datetime(year, month, day)
+    next_day = selected_day + timedelta(days=1)
+
 
     # establish database connection
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
@@ -51,9 +54,21 @@ def main():
             curs.execute("""
                 SELECT id, name, start_datetime, end_datetime
                 FROM appointments
+                WHERE start_datetime BETWEEN %(selected_day)s AND %(next_day)s
                 ORDER BY start_datetime;
-                """
+                """,
+                {
+                    'selected_day': selected_day,
+                    'next_day': next_day
+                }
             )
             rows = curs.fetchall()
 
             return render_template('main.html', rows=rows, form=form)
+
+
+# display main page
+@bp.route("/", methods=['GET', 'POST'])
+def main():
+    curr_date = datetime.now()
+    return redirect(url_for(".daily", year=curr_date.year, month=curr_date.month, day=curr_date.day))
