cb0ec7b65f104bdf6a1c5d0c28c3871bcec88e3e
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-03-26 20:36:42 -0500

    handling validation error for start/end date

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_wtf import FlaskForm
 from wtforms import (
     BooleanField,
@@ -7,10 +9,18 @@ from wtforms import (
     TextAreaField,
     TimeField,
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 
 class AppointmentForm(FlaskForm):
+    def validate_end_date(form, field):
+        start = datetime.combine(form.start_date.data, form.start_time.data)
+        end = datetime.combine(field.data, form.end_time.data)
+
+        if start >= end:
+            msg = 'End date/time must come after start date/time'
+            raise ValidationError(msg)
+
     name = StringField('Name', validators=[DataRequired()])
     start_date = DateField('StartDate', validators=[DataRequired()], format="%Y-%m-%d")
     start_time = TimeField('StartTime', validators=[DataRequired()], format="%H:%M")
